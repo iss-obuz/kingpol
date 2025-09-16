9dbe82942056cd035d3a9c639945c9d45c0fe93e
Szymon Talaga <stalaga@protonmail.com>
2025-09-16 14:18:19 +0200

    Fix handling of project paths, so they are not bound to the package directory.

--- kingpol/__init__.py
+++ kingpol/__init__.py
@@ -1,7 +1,9 @@
 from pathlib import Path
 
+from dvc.repo import Repo
+
 from .__about__ import __version__
 from .config import params
 from .dataset import PathsContainer
 
-paths = PathsContainer(Path(__file__).parent.parent / "data")
+paths = PathsContainer(Path(Repo().root_dir) / "data")
